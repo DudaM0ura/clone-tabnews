02fe793d41be8a67014e2aa012b6f78f7d7cbdda
Maria Eduarda Moura <93103711+DudaM0ura@users.noreply.github.com>
2025-11-04 15:04:11 +0000

    :tada: Adiciona a pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Oi mâe, nâo saia clicando e nâo confie em links que mandem no seu whatsApp, sorte que dessa vez era Eu e nâo um golpista</h1>
+}
+
+export default Home;
